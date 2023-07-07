6857923799a2b3581812777d9aa130b02619c283
bnjmn <bmcode19@gmail.com>
2023-07-07 01:18:02 +0100

    more commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const countdown = () => {
+    const countDate = new Date("August 21, 2023 00:00:00").getTime();
+
+    const now = new Date().getTime();
+    const gap = countDate - now
+    // console.log(gap)
+
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+
+    // calculate the above
+
+    const textDay = Math.floor(gap / day);
+    const textHour = Math.floor((gap % day) / hour);
+    const textMinute = Math.floor((gap % hour) / minute);
+    const textSecond = Math.floor((gap % minute) / second);
+
+
+
+    console.log(textSecond)
+
+    document.querySelector('.day').innerHTML = textDay;
+    document.querySelector('.hour').innerHTML = textHour;
+    document.querySelector('.minute').innerHTML = textMinute;
+    document.querySelector('.seconds').innerHTML = textSecond;
+}
+
+setInterval(countdown, 1000)
