b5ad84b254f7c0891295164be1631039762f4d17
bnjmn <bmcode19@gmail.com>
2024-01-06 23:53:03 +0100

    timer updated

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const countdown = () => {
-    const countDate = new Date("December 4, 2023 00:00:00").getTime();
+    const countDate = new Date("March 18, 2024 00:00:00").getTime();
 
     const now = new Date().getTime();
     const gap = countDate - now
